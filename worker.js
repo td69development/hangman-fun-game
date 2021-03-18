3a2bfdef83f2b9dcf6962eb154caaf851f05150d
72244878 <liamfighter2@gmail.com>
2021-03-18 08:52:36 +0800

    hangman sh*t game

---
+++ worker.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var cacheVersion = 1;
+var currentCache = {
+    offline: 'offline-cache' + cacheVersion
+};
+
+this.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(currentCache.offline).then(function(cache) {
+            return cache.addAll([
+                "offline.html",
+                "assets/css/main.css",
+                "assets/js/main.js"
+            ]);
+        })
+    );
+});
+
+this.addEventListener('fetch', event => {
+    if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+        event.respondWith(
+            fetch(event.request.url).catch(error => {
+                return caches.match("offline.html");
+            })
+        );
+    } else {
+        event.respondWith(caches.match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+        );
+    }
+});
